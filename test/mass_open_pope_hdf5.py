e3299a9f000f55f4f0493368a82a82ce921d944f
ggggggggg <galenoneil@gmail.com>
2017-03-15 15:36:03 -0600

    Test opening mass HDF5 only, and with lug files

--- test/mass_open_pope_hdf5.py
+++ test/mass_open_pope_hdf5.py
@@ -6,6 +6,7 @@ import numpy as np
 import h5py
 import os
 import sys
+import shutil
 
 
 def prep_pope_hdf5_for_mass(filename):
@@ -19,7 +20,7 @@ def prep_pope_hdf5_for_mass(filename):
             if "npulses" not in v.attrs: v.attrs["npulses"]=len(v["filt_value"])
 
 
-def open_pope_hdf5(filename):
+def open_pope_hdf5_only(filename):
     prep_pope_hdf5_for_mass(filename)
     data = mass.TESGroupHDF5(filename)
     cuts=mass.controller.AnalysisControl(
@@ -32,11 +33,33 @@ def open_pope_hdf5(filename):
     ds = data.first_good_dataset
     assert(ds.channum==13)
     assert(ds.nPulses==3038)
-    print("filename",ds.filename)
     return data
+
+def open_pope_hdf5_with_ljh(ljhfilename, noisefilename, hdf5_filename):
+    data = mass.TESGroup([ljhfilename], [noisefilename], hdf5_filename=hdf5_filename)
+    cuts=mass.controller.AnalysisControl(
+            pretrigger_rms=(None,30.),    # A cut against "big tails" from prior pulses
+            postpeak_deriv=(None, 20),
+            timestamp_diff_sec=(0.020, None)
+            )
+    data.apply_cuts(cuts)
+    ds = data.first_good_dataset
+    data.calibrate("p_filt_value",["MnKAlpha"])
+    assert(ds.channum==13)
+    assert(ds.nPulses==3038)
 
 
 if __name__ == "__main__":
-    print sys.argv
-    data = open_pope_hdf5(sys.argv[1])
-    print data
+    print sys.argv # arguments should be: hdf5_filename
+    hdf5_filename = sys.argv[1]
+    # open file with ljh files
+    # make a copy of the ljh file first, so that it doesn't affect the next test without ljh files
+    fname = os.path.expanduser("~/.julia/v0.5/ReferenceMicrocalFiles/ljh/20150707_D_chan13.ljh")
+    nfname = os.path.expanduser("~/.julia/v0.5/ReferenceMicrocalFiles/ljh/20150707_C_chan13.noi")
+    hdf5_filename_copy = hdf5_filename+"copy"
+    shutil.copy(hdf5_filename,hdf5_filename_copy)
+    data0 = open_pope_hdf5_with_ljh(fname, nfname, hdf5_filename_copy)
+
+    #open file without ljh files
+    data1 = open_pope_hdf5_only(hdf5_filename)
+    print data1

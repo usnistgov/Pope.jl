ad6d2943489ecf8c6fd3f6fbed94016c90044bd1
ggggggggg <galenoneil@gmail.com>
2017-12-05 15:03:50 -0700

    Try native :html format

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,18 @@ using Documenter, Pope, Pope.LJH
 makedocs(
     sitename = "Pope.jl",
     modules = [Pope, LJH],
-    # format = :html,
+    format = :html,
+    html_prettyurls = !("local" in ARGS),
+    # Pages = ["Home" => "index.md",
+    #          "LJH" => "ljh.md",
+    #          "Pope" => "pope.md"
+    #          ]
 )
 
 deploydocs(
     repo   = "github.com/usnistgov/Pope.jl.git",
     osname = "linux",
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = nothing, #Deps.pip("mkdocs", "python-markdown-math"),
     julia = "release"
 )
 

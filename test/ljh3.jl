2046fd1bef57e55495efb794607491e40047589f
ggggggggg <galenoneil@gmail.com>
2017-12-09 14:51:49 -0700

    Test indexes during writing

--- test/ljh3.jl
+++ test/ljh3.jl
@@ -2,9 +2,11 @@ using Pope: LJH
 using DataStructures
 using Base.Test
 
+@testset "ljh3" begin
+
 header_extra = OrderedDict(["a"=>"b","c"=>"d","e"=>Dict("ea"=>"eb")])
 fname = tempname()
-f = LJH.create3(fname, 9.6e-6, header_extra)
+fw = LJH.create3(fname, 9.6e-6, header_extra)
 
 traces = [rand(UInt16,rand(1:1000)) for i=1:10000];
 first_rising_samples = [rand(1:length(trace)) for trace in traces];
@@ -12,34 +14,35 @@ framecounts = 1:length(traces);
 timestamp_usecs = framecounts.*1000;
 
 for i = 1:length(traces)
-    write(f, traces[i],first_rising_samples[i],
+    write(fw, traces[i],first_rising_samples[i],
      framecounts[i], timestamp_usecs[i])
 end
-close(f)
+@test fw[1].data == traces[1]
+@test fw[77].data == traces[77]
+close(fw)
 
 f = LJH3File(fname)
 f2 = LJH3File(fname)
 records = [record for record in f];
-@testset "ljh3" begin
-    @test traces == [record.data for record in records]
-    @test first_rising_samples == [record.first_rising_sample for record in records]
-    @test collect(framecounts) == [record.framecount for record in records]
-    @test timestamp_usecs == [record.timestamp_usec for record in records]
-    @test records == collect(f)
-    @test f[1].data == traces[1]
-    @test f[77].data == traces[77]
-    @test all(f[key]==f.header[key] for key in keys(f))
-    @test all(f[key]==f.header[key] for key in keys(header_extra))
-    @test length(f2) == length(traces) # test that length works before collect is called on f2
-    @test f2.index == f.index
-end
+@test fw.index == f.index
+@test traces == [record.data for record in records]
+@test first_rising_samples == [record.first_rising_sample for record in records]
+@test collect(framecounts) == [record.framecount for record in records]
+@test timestamp_usecs == [record.timestamp_usec for record in records]
+@test records == collect(f)
+@test f[1].data == traces[1]
+@test f[77].data == traces[77]
+@test all(f[key]==f.header[key] for key in keys(f))
+@test all(f[key]==f.header[key] for key in keys(header_extra))
+@test length(f2) == length(traces) # test that length works before collect is called on f2
+@test f2.index == f.index
 close(f)
 close(f2)
+rm(fname)
+end
+
 
 
 # using BenchmarkTools
 # @benchmark read(seekstart(f.io)) setup=(f=LJH3File(fname)) teardown=close(f)
 # @benchmark collect(f) setup=(f=LJH3File(fname)) teardown=close(f)
-
-
-rm(fname)

2c0aae96fbca0b446a0fdf86f8ec05c32310061c
ggggggggg <galenoneil@gmail.com>
2017-12-09 14:39:24 -0700

    rowcount->framecount, consolidate some functions

--- src/ljh3.jl
+++ src/ljh3.jl
@@ -3,7 +3,7 @@ import Base: ==
 
 struct LJH3File
     io::IOStream
-    index::Vector{Int} # 1 more than number of records
+    index::Vector{Int} # points to the start of records, and potential next record
     frametime::Float64
     header::OrderedDict{String,Any}
 end
@@ -19,17 +19,17 @@ end
 struct LJH3Record
     data::Vector{UInt16}
     first_rising_sample::UInt32
-    rowcount::Int64
+    framecount::Int64
     timestamp_usec::Int64
 end
 data(r::LJH3Record) = r.data
-rowcount(r::LJH3Record) = r.rowcount
+framecount(r::LJH3Record) = r.framecount
 timestamp_usec(r::LJH3Record) = r.timestamp_usec
 
-==(a::LJH3Record, b::LJH3Record) = a.data == b.data && a.first_rising_sample == b.first_rising_sample && a.rowcount == b.rowcount && a.timestamp_usec == b.timestamp_usec
+==(a::LJH3Record, b::LJH3Record) = a.data == b.data && a.first_rising_sample == b.first_rising_sample && a.framecount == b.framecount && a.timestamp_usec == b.timestamp_usec
 Base.close(ljh::LJH3File) = close(ljh.io)
-function Base.write(ljh::LJH3File, trace::Vector{UInt16},first_rising_sample, rowcount::Int64, timestamp_usec::Int64)
-    write(ljh.io, UInt32(length(trace)), UInt32(first_rising_sample), rowcount, timestamp_usec, trace)
+function Base.write(ljh::LJH3File, trace::Vector{UInt16},first_rising_sample, framecount::Int64, timestamp_usec::Int64)
+    write(ljh.io, UInt32(length(trace)), UInt32(first_rising_sample), framecount, timestamp_usec, trace)
     push!(ljh.index,position(ljh.io))
 end
 function create3(filename::AbstractString, frametime, header_extra = Dict();version="3.0.0")
@@ -54,37 +54,34 @@ end
 function _readrecord(ljh::LJH3File,i)
     trace_samples = read(ljh.io, UInt32)
     first_rising_sample = read(ljh.io, UInt32)
-    rowcount = read(ljh.io, Int64)
+    framecount = read(ljh.io, Int64)
     timestamp_usec = read(ljh.io, Int64)
     data = read(ljh.io, UInt16, trace_samples)
     if i==length(ljh.index)
         deltapos = 2*trace_samples + 24
         @inbounds push!(ljh.index,ljh.index[i]+deltapos)
+        # now, ljh.index[end] is the first unobserved byte offset into ljh.io
+        # it may or may not be the start of a pulse
     end
-    LJH3Record(data, first_rising_sample, rowcount, timestamp_usec)
-end
-function index!(ljh::LJH3File)
-    if stat(ljh.io).size > ljh.index[end]
-        collect(ljh)
-    end
-    return nothing
-end
-function ljh_number_of_records(ljh::LJH3File)
-    index!(ljh)
-    return length(ljh.index)-1
+    LJH3Record(data, first_rising_sample, framecount, timestamp_usec)
 end
 # Array interface
 function Base.getindex(ljh::LJH3File,index::Int)
     seekto(ljh, index)
     _readrecord(ljh,index)
 end
-Base.size(ljh::LJH3File) = (ljh_number_of_records(ljh),)
-Base.length(ljh::LJH3File) = ljh_number_of_records(ljh)
-Base.endof(ljh::LJH3File) = ljh_number_of_records(ljh)
+Base.size(ljh::LJH3File) = (length(ljh),)
+function Base.length(ljh::LJH3File)
+    # iterate from the last entry in index to the end of the file
+    # to fill ljh.index
+    state = length(ljh.index), stat(ljh.io).size
+    while !done(ljh,state)
+        _,state = next(ljh,state)
+    end
+    return length(ljh.index)-1
+end
+Base.endof(ljh::LJH3File) = length(ljh)
 # iterator interface
-# Base.start(ljh::LJH3File) = (seekto(ljh,1);1)
-# Base.next(ljh::LJH3File,j) = pop!(ljh),j+1
-# Base.done(ljh::LJH3File,j) = ljh.index[end]==stat(ljh.io).size
 Base.start(ljh::LJH3File) = (seekto(ljh,1);(1,stat(ljh.io).size))
 function Base.next(ljh::LJH3File,state)
     j,sz=state
@@ -94,7 +91,6 @@ function Base.done(ljh::LJH3File,state)
     j,sz=state
     ljh.index[j]==sz
 end
-
 Base.iteratorsize(ljh::LJH3File) = Base.SizeUnknown()
 # getindex with strings
 Base.getindex(ljh::LJH3File,key::AbstractString) = ljh.header[key]
--- test/ljh3.jl
+++ test/ljh3.jl
@@ -8,12 +8,12 @@ f = LJH.create3(fname, 9.6e-6, header_extra)
 
 traces = [rand(UInt16,rand(1:1000)) for i=1:10000];
 first_rising_samples = [rand(1:length(trace)) for trace in traces];
-rowcounts = 1:length(traces);
-timestamp_usecs = rowcounts.*1000;
+framecounts = 1:length(traces);
+timestamp_usecs = framecounts.*1000;
 
 for i = 1:length(traces)
     write(f, traces[i],first_rising_samples[i],
-     rowcounts[i], timestamp_usecs[i])
+     framecounts[i], timestamp_usecs[i])
 end
 close(f)
 
@@ -23,7 +23,7 @@ records = [record for record in f];
 @testset "ljh3" begin
     @test traces == [record.data for record in records]
     @test first_rising_samples == [record.first_rising_sample for record in records]
-    @test collect(rowcounts) == [record.rowcount for record in records]
+    @test collect(framecounts) == [record.framecount for record in records]
     @test timestamp_usecs == [record.timestamp_usec for record in records]
     @test records == collect(f)
     @test f[1].data == traces[1]

33ab8a3efb576ba0c4d4cbdde97e96c89743d4b8
ggggggggg <galenoneil@gmail.com>
2017-12-08 20:53:38 -0700

    Call position less often, possibly faster

--- src/ljh3.jl
+++ src/ljh3.jl
@@ -1,6 +1,7 @@
 using JSON
+import Base: ==
 
-mutable struct LJH3File
+struct LJH3File
     io::IOStream
     index::Vector{Int} # 1 more than number of records
     frametime::Float64
@@ -25,6 +26,7 @@ data(r::LJH3Record) = r.data
 rowcount(r::LJH3Record) = r.rowcount
 timestamp_usec(r::LJH3Record) = r.timestamp_usec
 
+==(a::LJH3Record, b::LJH3Record) = a.data == b.data && a.first_rising_sample == b.first_rising_sample && a.rowcount == b.rowcount && a.timestamp_usec == b.timestamp_usec
 Base.close(ljh::LJH3File) = close(ljh.io)
 function Base.write(ljh::LJH3File, trace::Vector{UInt16},first_rising_sample, rowcount::Int64, timestamp_usec::Int64)
     write(ljh.io, UInt32(length(trace)), UInt32(first_rising_sample), rowcount, timestamp_usec, trace)
@@ -49,15 +51,15 @@ function seekto(ljh::LJH3File, i::Int)
         error("LJH3 Bounds Error")
     end
 end
-function Base.pop!(ljh::LJH3File)
+function _readrecord(ljh::LJH3File,i)
     trace_samples = read(ljh.io, UInt32)
     first_rising_sample = read(ljh.io, UInt32)
     rowcount = read(ljh.io, Int64)
     timestamp_usec = read(ljh.io, Int64)
     data = read(ljh.io, UInt16, trace_samples)
-    pos = position(ljh.io)
-    if pos>ljh.index[end]
-        push!(ljh.index,pos)
+    if i==length(ljh.index)
+        deltapos = 2*trace_samples + 24
+        @inbounds push!(ljh.index,ljh.index[i]+deltapos)
     end
     LJH3Record(data, first_rising_sample, rowcount, timestamp_usec)
 end
@@ -74,15 +76,25 @@ end
 # Array interface
 function Base.getindex(ljh::LJH3File,index::Int)
     seekto(ljh, index)
-    pop!(ljh)
+    _readrecord(ljh,index)
 end
 Base.size(ljh::LJH3File) = (ljh_number_of_records(ljh),)
 Base.length(ljh::LJH3File) = ljh_number_of_records(ljh)
 Base.endof(ljh::LJH3File) = ljh_number_of_records(ljh)
 # iterator interface
-Base.start(ljh::LJH3File) = (seekto(ljh,1);1)
-Base.next(ljh::LJH3File,j) = pop!(ljh),j+1
-Base.done(ljh::LJH3File,j) = ljh.index[end]==stat(ljh.io).size
+# Base.start(ljh::LJH3File) = (seekto(ljh,1);1)
+# Base.next(ljh::LJH3File,j) = pop!(ljh),j+1
+# Base.done(ljh::LJH3File,j) = ljh.index[end]==stat(ljh.io).size
+Base.start(ljh::LJH3File) = (seekto(ljh,1);(1,stat(ljh.io).size))
+function Base.next(ljh::LJH3File,state)
+    j,sz=state
+    _readrecord(ljh,j),(j+1,sz)
+end
+function Base.done(ljh::LJH3File,state)
+    j,sz=state
+    ljh.index[j]==sz
+end
+
 Base.iteratorsize(ljh::LJH3File) = Base.SizeUnknown()
 # getindex with strings
 Base.getindex(ljh::LJH3File,key::AbstractString) = ljh.header[key]
--- test/ljh3.jl
+++ test/ljh3.jl
@@ -6,10 +6,10 @@ header_extra = OrderedDict(["a"=>"b","c"=>"d","e"=>Dict("ea"=>"eb")])
 fname = tempname()
 f = LJH.create3(fname, 9.6e-6, header_extra)
 
-traces = [rand(UInt16,rand(1:1000)) for i=1:100]
-first_rising_samples = [rand(1:length(trace)) for trace in traces]
-rowcounts = 1:length(traces)
-timestamp_usecs = rowcounts.*1000
+traces = [rand(UInt16,rand(1:1000)) for i=1:10000];
+first_rising_samples = [rand(1:length(trace)) for trace in traces];
+rowcounts = 1:length(traces);
+timestamp_usecs = rowcounts.*1000;
 
 for i = 1:length(traces)
     write(f, traces[i],first_rising_samples[i],
@@ -19,12 +19,13 @@ close(f)
 
 f = LJH3File(fname)
 f2 = LJH3File(fname)
-records = [record for record in f]
+records = [record for record in f];
 @testset "ljh3" begin
     @test traces == [record.data for record in records]
     @test first_rising_samples == [record.first_rising_sample for record in records]
     @test collect(rowcounts) == [record.rowcount for record in records]
     @test timestamp_usecs == [record.timestamp_usec for record in records]
+    @test records == collect(f)
     @test f[1].data == traces[1]
     @test f[77].data == traces[77]
     @test all(f[key]==f.header[key] for key in keys(f))
@@ -34,4 +35,11 @@ records = [record for record in f]
 end
 close(f)
 close(f2)
+
+
+# using BenchmarkTools
+# @benchmark read(seekstart(f.io)) setup=(f=LJH3File(fname)) teardown=close(f)
+# @benchmark collect(f) setup=(f=LJH3File(fname)) teardown=close(f)
+
+
 rm(fname)

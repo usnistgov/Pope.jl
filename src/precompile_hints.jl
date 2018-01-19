cc2f6d5b20a808b42372abdc854e975054558161
ggggggggg <galenoneil@gmail.com>
2018-01-19 11:28:45 -0700

    deal with artifacts dir in precompile_hints

--- src/precompile_hints.jl
+++ src/precompile_hints.jl
@@ -1,4 +1,9 @@
 cd(joinpath(@__DIR__,"..","test")) do
+    # clean out the artifacts directory
+    artifacts = joinpath(@__DIR__,"..","test","artifacts")
+    isdir(artifacts) && rm(artifacts,recursive=true)
+    mkdir(artifacts)
+
     include(joinpath(@__DIR__,"..","test","noise_analysis.jl"))
     include(joinpath(@__DIR__,"..","test","ljh.jl"))
     include(joinpath(@__DIR__,"..","test","ljh3.jl"))

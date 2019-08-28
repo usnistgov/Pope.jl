6e525fe488f2c4207bfbbd6a138e95f97dd61cae
Joe Fowler <drjoefowler@gmail.com>
2019-08-28 17:25:52 -0600

    Try to have travis develop ARMA and Ref...

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,5 @@
 # it should be removed once we understand how to make our own NIST registry.
 
 @info "Running deps/build.jl"
-Pkg.add(Pkg.PackageSpec(name="ReferenceMicrocalFiles", PackageSpec(url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"))
-Pkg.add(Pkg.PackageSpec(name="ARMA", PackageSpec(url="https://github.com/joefowler/ARMA.jl"))
-
-isdir(expanduser("~/.daq")) || mkdir(expanduser("~/.daq"))
+# Pkg.add(Pkg.PackageSpec(name="ReferenceMicrocalFiles", PackageSpec(url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"))
+# Pkg.add(Pkg.PackageSpec(name="ARMA", PackageSpec(url="https://github.com/joefowler/ARMA.jl"))

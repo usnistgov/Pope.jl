f6efa7c92e49856f195984222d1aaa8639e4d57a
Joseph Fowler <drjoefowler@gmail.com>
2019-08-28 12:46:57 -0600

    Update build.jl
    Attempt to make Travis work with unregistered packages....

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,16 @@
 # this is a hack to allow POPE to depend on unregistered Julia Packages
-# it should be removed if/when there is an official way of doing this other
-# than maintaining and using my own copy of METADATA
+# it should be removed once we understand how to make our own NIST registry.
 
-if !isdir(Pkg.dir("ReferenceMicrocalFiles"))
-	Pkg.clone("https://github.com/ggggggggg/ReferenceMicrocalFiles.jl")
+print("Running deps/build.jl")
+try
+	import ReferenceMicrocalFiles
+catch
+	Pkg.add(PackageSpec(url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"))
 end
-if !isdir(Pkg.dir("ARMA"))
-	Pkg.clone("https://github.com/joefowler/ARMA.jl")
+try
+	import ARMA
+catch
+	Pkg.add(PackageSpec(url="https://github.com/joefowler/ARMA.jl"))
 end
 
 isdir(expanduser("~/.daq")) || mkdir(expanduser("~/.daq"))

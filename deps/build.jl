37415877c2c2020e95b601266873876957f8c2e9
Joseph Fowler <drjoefowler@gmail.com>
2019-08-28 13:35:32 -0600

    Try travis with build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,8 @@
 # this is a hack to allow POPE to depend on unregistered Julia Packages
 # it should be removed once we understand how to make our own NIST registry.
 
-print("Running deps/build.jl")
-try
-	import ReferenceMicrocalFiles
-catch
-	Pkg.add(PackageSpec(url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"))
-end
-try
-	import ARMA
-catch
-	Pkg.add(PackageSpec(url="https://github.com/joefowler/ARMA.jl"))
-end
+@info "Running deps/build.jl"
+Pkg.add(Pkg.PackageSpec(name="ReferenceMicrocalFiles", PackageSpec(url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"))
+Pkg.add(Pkg.PackageSpec(name="ARMA", PackageSpec(url="https://github.com/joefowler/ARMA.jl"))
 
 isdir(expanduser("~/.daq")) || mkdir(expanduser("~/.daq"))

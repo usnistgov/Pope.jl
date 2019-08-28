87856e544afa27f1fb3ae50ac4591801a6781ace
Joe Fowler <drjoefowler@gmail.com>
2019-08-28 17:16:32 -0600

    Try this Travis

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@
 # it should be removed once we understand how to make our own NIST registry.
 
 @info "Running deps/build.jl"
-Pkg.add(Pkg.PackageSpec(name="ReferenceMicrocalFiles", PackageSpec(url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"))
-Pkg.add(Pkg.PackageSpec(name="ARMA", PackageSpec(url="https://github.com/joefowler/ARMA.jl"))
-
-isdir(expanduser("~/.daq")) || mkdir(expanduser("~/.daq"))
+Pkg.develop([
+    Pkg.PackageSpec(name="ReferenceMicrocalFiles", url="https://github.com/ggggggggg/ReferenceMicrocalFiles.jl"),
+    Pkg.PackageSpec(name="ARMA", url="https://github.com/joefowler/ARMA.jl"),
+    ])

bb8ef3537623974d3a1fc77a5d26552598e6cc39
ggggggggg <galen.oneil@nist.gov>
2019-09-03 10:37:36 -0600

    use even more bash to get the correct directory

--- scripts/debugscript.jl
+++ scripts/debugscript.jl
@@ -1,6 +1,23 @@
-#!/usr/bin/env julia --project --color=yes --startup-file=no
-
+#!/bin/bash
+#=
+JULIA="${JULIA:-julia}"
+JULIA_CMD="${JULIA_CMD:-$JULIA --color=yes --startup-file=no}"
+# below gets the directory name of the script, even if there is a symlink involved
+# from https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+export JULIA_PROJECT=$DIR/..
+export JULIA_LOAD_PATH=@:@stdlib  # exclude default environment
+exec $JULIA_CMD -e 'include(popfirst!(ARGS))' "${BASH_SOURCE[0]}" "$@"
+=#
 tstart = time()
+@show get(ENV,"JULIA_PROJECT", nothing)
+@show get(ENV,"JULIA_LOAD_PATH", nothing)
 println("before using ArgParse ", time()-tstart)
 using ArgParse
 println("after using ArgParse ",time()-tstart)

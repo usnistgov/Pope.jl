f0e9a685950ace9d7c13a36e545f99734aa80cba
ggggggggg <galen.oneil@nist.gov>
2019-09-03 09:51:52 -0600

    use --project

--- scripts/debugscript.jl
+++ scripts/debugscript.jl
@@ -1,14 +1,9 @@
-#!/bin/bash
-#=
-JULIA="${JULIA:-julia}"
-JULIA_CMD="${JULIA_CMD:-$JULIA --color=yes --startup-file=no}"
-export JULIA_PROJECT="$(pwd)/$JULIA_PROJECT.."
-export JULIA_LOAD_PATH=@:@stdlib  # exclude default environment
-exec $JULIA_CMD -e 'include(popfirst!(ARGS))' "${BASH_SOURCE[0]}" "$@"
-=#
-@show ENV["JULIA_PROJECT"]
-@show ENV["JULIA_LOAD_PATH"]
+#!/usr/bin/env julia --project --color=yes --startup-file=no
+
+tstart = time()
+println("before using ArgParse ", time()-tstart)
 using ArgParse
+println("after using ArgParse ",time()-tstart)
 s = ArgParseSettings()
 @add_arg_table s begin
     "--outputfile", "-o"
@@ -16,7 +11,8 @@ s = ArgParseSettings()
         help="specify the path of the outputfile, otherwise it will make one up based on pulse_file"
 
 end
+println("after add_arg_table ",time()-tstart)
 parsed_args = parse_args(ARGS, s)
-println(parsed_args)
+@show parsed_args
 using Pope
-println("your other scripts should work!")
+println("your other scripts should work! ",time()-tstart)
--- scripts/ljh2off.jl
+++ scripts/ljh2off.jl
@@ -1,11 +1,4 @@
-#!/bin/bash
-#=
-JULIA="${JULIA:-julia}"
-JULIA_CMD="${JULIA_CMD:-$JULIA --color=yes --startup-file=no}"
-export JULIA_PROJECT="$(pwd)/$JULIA_PROJECT.."
-export JULIA_LOAD_PATH=@:@stdlib  # exclude default environment
-exec $JULIA_CMD -e 'include(popfirst!(ARGS))' "${BASH_SOURCE[0]}" "$@"
-=#
+#!/usr/bin/env julia --project --color=yes --startup-file=no
 
 using ArgParse
 s = ArgParseSettings(description="Coallate ljh files and convert to off with given model file\n"*
